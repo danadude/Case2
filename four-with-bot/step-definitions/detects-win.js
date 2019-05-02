abbb5c28c95eaa753c5024b842f9ddb03bfa9f22
Danadude <dan.olsen85@gmail.com>
2019-05-02 16:57:38 +0200

    end-of-day-update

--- four-with-bot/step-definitions/detects-win.js
+++ four-with-bot/step-definitions/detects-win.js
@@ -1,6 +1,8 @@
 let {$, sleep, clickRow} = require('./funcs');
  
 let sleepTime = 500;
+let gameSeq = []
+let i
 
 module.exports = function(){
  
@@ -24,126 +26,77 @@ module.exports = function(){
           break;
         }
       }
-      await sleep(sleepTime * 2);
+      await sleep(sleepTime);
     }
   });
  
   this.When(/^with two different names$/, async function () {
     let inputFields = await $('input[placeholder="Namn (2-10 tecken)"]');
     await inputFields[0].sendKeys('Spelare 1');
-    await sleep(sleepTime * 2);
+    await sleep(sleepTime);
     await inputFields[1].sendKeys('Spelare 2');
-    await sleep(sleepTime * 2);
+    await sleep(sleepTime);
   });
  
   this.When(/^press the Börja spela\-button$/, async function () {
     let beginButton = await $('.begin-btn');
     beginButton.click();
-    await sleep(sleepTime * 2);
+    await sleep(sleepTime);
   });
  
   this.Then(/^the game should start$/, async function () {
     let activeMenuLink = await $('.nav-link.active');
     let text = await activeMenuLink.getText();
-    await sleep(1000); // small wait needed
+    await sleep(sleepTime); // small wait needed
     assert.equal(text, 'Avbryt spelet', 'The game did not start!');
-    await sleep(sleepTime * 2);
+    await sleep(sleepTime);
   });
  
   // Scenarios
  
   this.When(/^the first player plays (\d+) bricks in a row horizontally$/, async function (brickstoWin) {
- 
-    // NOTE: Only began this code, by playing one brick
-    // let gameInfo = await driver.findElement(by.css('html > body > div > main.container.mt-4.mb-3 > div.game > div.game-info > h3.mb-3.text-center > span'))
-    // clicking slots[0] is putting a coin in column 1
-    // clicking slots[1] is putting a coin in column 2
-    // loop this shit!!!!
-    // DOM need to regrab element.... DOM changed?
-    await clickRow(1)
-    await clickRow(6)
-    await clickRow(2)
-    await clickRow(6)
-    await clickRow(3)
-    await clickRow(6)
-    await clickRow(4)
-    // Game won how do i assert
-    await sleep(sleepTime * 2)
- 
- 
+    // Loops the game sequence required to get the desired outcome
+    gameSeq = [1,6,2,6,3,6,4]
+    for(i = 0; i < gameSeq.length; i++){
+      await clickRow(gameSeq[i])
+    }
   });
 
   this.Then(/^he\/she should win$/,async function () {
-  let gameInfo = await driver.findElement(by.css('html > body > div > main > div.game > div.game-info > h3.mb-3 > span')).getText()
+  //let gameInfo = await driver.findElement(by.css('html > body > div > main > div.game > div.game-info > h3.mb-3 > span')).getText()
+  let gameInfo = await driver.findElement(by.css('h3 > span')).getText()
   let winMessage = 'Spelare \d vann, efter \d drag!'
   await gameInfo
+  console.log(gameInfo)
   // asserting agianst the text on the page
   assert(gameInfo === winMessage, 'Matchar EJ')
-
   });
 
   this.When(/^the first player plays (\d+) bricks in a row vertical$/,async function (brickstoWin) {
-    let slots = await $('.slot')
-    // let gameInfo = await driver.findElement(by.css('html > body > div > main.container.mt-4.mb-3 > div.game > div.game-info > h3.mb-3.text-center > span'))
-    // clicking slots[0] is putting a coin in column 1
-    // clicking slots[1] is putting a coin in column 2
-    // loop this shit!!!!
-    // DOM need to regrab element.... DOM changed?
-    await clickRow(1)
-    await clickRow(6)
-    await clickRow(2)
-    await clickRow(6)
-    await clickRow(3)
-    await clickRow(6)
-    await clickRow(1)
-    await clickRow(6)
+    // Loops the game sequence required to get the desired outcome
     // Game won how do i assert
-    await sleep(sleepTime * 2)
- 
- 
+    gameSeq = [1,6,2,6,3,6,1,6]
+    for(i = 0; i < gameSeq.length; i++){
+      await clickRow(gameSeq[i])
+    }
   });
 
   this.When(/^the first player plays (\d+) bricks in a diagonally \(left to right\)$/,async function (brickstoWin) {
-    let slots = await $('.slot')
-    // let gameInfo = await driver.findElement(by.css('html > body > div > main.container.mt-4.mb-3 > div.game > div.game-info > h3.mb-3.text-center > span'))
-    // clicking slots[0] is putting a coin in column 1
-    // clicking slots[1] is putting a coin in column 2
-    // loop this shit!!!!
-    // DOM need to regrab element.... DOM changed?
-    await clickRow(0)
-    await clickRow(1)
-    await clickRow(1)
-    await clickRow(2)
-    await clickRow(2)
-    await clickRow(3)
-    await clickRow(2)
-    await clickRow(3)
-    await clickRow(3)
-    await clickRow(1)
-    await clickRow(3)
+    // Loops the game sequence required to get the desired outcome
     // Game won how do i assert
-    await sleep(sleepTime * 4)
+    gameSeq = [0,1,1,2,2,3,2,3,3,1,3]
+    for(i = 0; i < gameSeq.length; i++){
+      await clickRow(gameSeq[i])
+    }
   });
 
   this.When(/^the first player plays (\d+) bricks in a diagonally \(right to left\)$/,async function (brickstoWin) {
-    let slots = await $('.slot')
-    // clicking slots[0] is putting a coin in column 1
-    // clicking slots[1] is putting a coin in column 2
-    // loop this shit!!!!
-    // DOM need to regrab element.... DOM changed?
-    await clickRow(6)
-    await clickRow(5)
-    await clickRow(5)
-    await clickRow(4)
-    await clickRow(4)
-    await clickRow(3)
-    await clickRow(4)
-    await clickRow(3)
-    await clickRow(3)
-    await clickRow(1)
-    await clickRow(3)
+    // Loops the game sequence required to get the desired outcome
     // Game won how do i assert
-    await sleep(sleepTime * 4)
+    gameSeq = [6,5,5,4,4,3,4,3,3,1,3]
+    for(i = 0; i < gameSeq.length; i++){
+      await clickRow(gameSeq[i])
+    }
   });
 
 
