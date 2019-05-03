294c1052f122bcfa899cb207372ae6a24ffd0b62
tomasmansson <tomas.mansson93@gmail.com>
2019-05-03 09:38:16 +0200

    Tomas US05

---
+++ four-with-bot/features/US05.feature
@@ -0,0 +1,23 @@
+# [US05] As a user I want the normal bot to have some randomness in its game choices, so that I can improve my own game
+# Tomas
+
+Feature: Check normal bot randomness
+
+  Background: Human vs. normal bot, Game should start
+    Given that I goto the game page
+    When I choose to play as a human player vs. a normal bot
+    And I enter two different names
+    And I press the Börja spela-button
+    Then the game should start
+
+  Scenario: Game 1
+    When I place my coins in a certain way
+    Then the normal bot should place its coins in a certain way
+
+  Scenario: Game 2
+    When I place my coins the same way as the previous game
+    Then the normal bot should vary his game choice
+
+
+
+# Kör även normal bot vs normal bot?
---
+++ four-with-bot/step-definitions/US05.js
@@ -0,0 +1,49 @@
+// [US05] As a user I want the normal bot to have some randomness in its game choices, so that I can improve my own game
+// Tomas
+
+let { $, sleep } = require('./funcs');
+
+let sleepTime = 500;
+
+async function klick(row) {
+  slots = await $('.slot'),
+    await slots[row].click(),
+    await sleep(sleepTime / 2)
+}
+
+module.exports = function () {
+
+  this.Given(/^that I goto the game page$/, async function () {
+    await helpers.loadPage('http://localhost:3000/game');
+  });
+
+  this.When(/^I choose to play as a human player vs\. a normal bot$/, async function () {
+    let typeChoiceButton = await $('.type-choice-btn');
+    await typeChoiceButton[0].click();
+    let choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
+    for (let choice of choices) {
+      let text = await choice.getText();
+      if (text === 'Bot') {
+        await choice.click();
+        // we MUST break because the dom changes after click
+        // and erases the old menu.. (tricky...)
+        break;
+      }
+    }
+    await typeChoiceButton[1].click();
+    choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
+    for (let choice of choices) {
+      text = await choice.getText();
+      if (text === 'Människa') {
+        await choice.click();
+        // we MUST break because the dom changes after click
+        // and erases the old menu.. (tricky...)
+        break;
+      }
+    }
+    await sleep(sleepTime * 2);
+
+  });
+
+
+}
