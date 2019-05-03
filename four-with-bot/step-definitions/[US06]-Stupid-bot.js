9dad3720b37e7f5a789b7b1cabee34a25dba07d5
eyeq1337 <eyeq24@gmail.com>
2019-05-03 11:20:05 +0200

    us06-05

--- four-with-bot/step-definitions/[US06]-Stupid-bot.js
+++ four-with-bot/step-definitions/[US06]-Stupid-bot.js
@@ -16,46 +16,52 @@ module.exports = function () {
     let choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
     for (let choice of choices) {
       let text = await choice.getText();
-      if (text === 'Dum bot') {
-        await choice.click();
-        // we MUST break because the dom changes after click
-        // and erases the old menu.. (tricky...)
-        break;
-      }
-    }
-    await typeChoiceButton[1].click();
-    choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
-    for (let choice of choices) {
-      text = await choice.getText();
       if (text === 'Bot') {
         await choice.click();
         // we MUST break because the dom changes after click
         // and erases the old menu.. (tricky...)
         break;
       }
+    }
 
-      {
+    await typeChoiceButton[1].click();
+    choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
+    for (let choice of choices) {
+      text = await choice.getText();
+      if (text === 'Dum Bot') {
+        await choice.click();
+        // we MUST break because the dom changes after click
+        // and erases the old menu.. (tricky...)
+        break;
+      }
+    }
+    await sleep(sleepTime * 2);
+  });
 
-        this.When(/^with two different names$/, async function () {
-          let inputFields = await $('input[placeholder="Namn (2-10 tecken)"]');
-          await inputFields[0].sendKeys('Spelare 1');
-          await sleep(sleepTime * 2);
-          await inputFields[1].sendKeys('Spelare 2');
-          await sleep(sleepTime * 2);
-        });
+  this.When(/^with two different names$/, async function () {
+    let inputFields = await $('input[placeholder="Namn (2-10 tecken)"]');
+    await inputFields[0].sendKeys('Spelare 1');
+    await sleep(sleepTime * 2);
+    await inputFields[1].sendKeys('Spelare 2');
+    await sleep(sleepTime * 2);
+  });
 
-        this.When(/^press the Börja spela\-button$/, async function () {
-          let beginButton = await $('.begin-btn');
-          beginButton.click();
-          await sleep(sleepTime * 2);
-        });
+  this.When(/^press the Börja spela\-button$/, async function () {
+    let beginButton = await $('.begin-btn');
+    beginButton.click();
+    await sleep(sleepTime * 2);
+  });
 
-        this.Then(/^the game should start$/, async function () {
-          let activeMenuLink = await $('.nav-link.active');
-          let text = await activeMenuLink.getText();
-          await sleep(1000); // small wait needed
-          assert.equal(text, 'Avbryt spelet', 'The game did not start!');
-          await sleep(sleepTime * 2);
-        });
+  this.Then(/^the game should start$/, async function () {
+    let activeMenuLink = await $('.nav-link.active');
+    let text = await activeMenuLink.getText();
+    await sleep(1000); // small wait needed
+    assert.equal(text, 'Avbryt spelet', 'The game did not start!');
+    await sleep(sleepTime * 2);
+  });
 
-        // Scenarios
+
+
+
+
+}
