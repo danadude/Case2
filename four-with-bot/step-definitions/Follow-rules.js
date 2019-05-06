705529d7f10f01b1ac20e39a2258df0b085a2f1e
Danadude <dan.olsen85@gmail.com>
2019-05-06 14:01:54 +0200

    D_arbete_mandag

---
+++ four-with-bot/step-definitions/Follow-rules.js
@@ -0,0 +1,23 @@
+let { $, sleep, clickRow } = require("./funcs");
+
+let sleepTime = 500;
+
+module.exports = function() {
+    this.When(/^"([^"]*)" plays a bricks$/,async function (arg1) {
+      let gameInfo = await driver.findElement(by.css('h3')).getText()
+      await gameInfo
+      assert(gameInfo.includes('Spelare 1, drag 1') , 'Boten förlorade mot idioten!')
+      clickRow(1)
+
+      });
+
+      this.Then(/^it should be "([^"]*)" turn$/,async function (arg1) {
+        await sleep(sleepTime*6)
+        let gameInfo = await driver.findElement(by.css('h3')).getText()
+        await gameInfo
+        assert(gameInfo.includes('Spelare 2, drag 1') , 'Boten förlorade mot idioten!')
+
+      });      
+
+
+};
--- four-with-bot/step-definitions/[US06]-Stupid-bot.js
+++ four-with-bot/step-definitions/[US06]-Stupid-bot.js
@@ -2,20 +2,13 @@
 // David
 
 let { $, sleep } = require('./funcs');
-
+let spelare1 = 0
+let spelare2 = 0
 let sleepTime = 500;
-let spelare1 = 'Normal bot'
-let spelare2 = 'Dumb bot'
 
 module.exports = function () {
 
   // Background
-
-  this.Given(/^that I goto the game page$/, async function () {
-    await helpers.loadPage('http://localhost:3000/game');
-    await sleep(sleepTime * 2);
-
-  });
 
   this.When(/^I choose to play as a dumb bot player vs\. a normal bot$/, async function () {
     let typeChoiceButton = await $('.type-choice-btn');
@@ -24,7 +17,7 @@ module.exports = function () {
     let choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
     for (let choice of choices) {
       let text = await choice.getText();
-      if (text === 'Bot') {
+      if (text.includes("Dum")) {
         await choice.click();
         // we MUST break because the dom changes after click
         // and erases the old menu.. (tricky...)
@@ -36,79 +29,67 @@ module.exports = function () {
     choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
     for (let choice of choices) {
       text = await choice.getText();
-      if (text === 'Dum Bot') {
+      if (text === 'Bot') {
         await choice.click();
         // we MUST break because the dom changes after click
         // and erases the old menu.. (tricky...)
         break;
       }
-<<<<<<< HEAD
-    }
-    await sleep(sleepTime * 2);
-  });
-=======
       await sleep(sleepTime * 2);
     }
     });
->>>>>>> bf81c80ff4c15f62e150bdae4ac49b62f22ee1ff
 
-  this.When(/^with two different names$/, async function () {
-    let inputFields = await $('input[placeholder="Namn (2-10 tecken)"]');
-    await inputFields[0].sendKeys('Spelare 1');
-    await sleep(sleepTime * 2);
-    await inputFields[1].sendKeys('Spelare 2');
-    await sleep(sleepTime * 2);
-  });
-
-  this.When(/^press the Börja spela\-button$/, async function () {
-    let beginButton = await $('.begin-btn');
-    beginButton.click();
-    await sleep(sleepTime * 2);
-  });
-
-  this.Then(/^the game should start$/, async function () {
-    let activeMenuLink = await $('.nav-link.active');
-    let text = await activeMenuLink.getText();
-    await sleep(200); // small wait needed
-    assert.equal(text, 'Avbryt spelet', 'The game did not start!');
-    await sleep(sleepTime * 2);
-  });
-<<<<<<< HEAD
-  // Scenario: Game
-
-  this.Then(/^they should play (\d+) games against each other$/, async function (brickstoWin) {
-    // Write code here that turns the phrase above into concrete actions
-    await sleep(sleepTime * 10000);
+  this.Then(/^they should play (\d+) games against each other$/,{timeout: 90*1000}, async function (gamesToPlay) {
+    let gameInfo = await driver.findElement(by.css('h3')).getText()
+      do {
+      await sleep(sleepTime*2)
+      gameInfo = await driver.findElement(by.css('h3')).getText()
+      }
+      while (! gameInfo.includes('vann'))
+      if (gameInfo.includes('Spelare 2')){
+      spelare2 ++
+      }
+      if (gameInfo.includes('Spelare 1')){
+      spelare1 ++
+      }
     let againButton = await $('.again-btn');
     await againButton.click();
+    await sleep(2000)
+    let beginButton = await $('.begin-btn');
+    await beginButton.click();
+    await sleep(sleepTime*2);
+    do {
+      await sleep(sleepTime*2)
+      gameInfo = await driver.findElement(by.css('h3')).getText()
+      }
+      while (! gameInfo.includes('vann'))
+      if (gameInfo.includes('Spelare 2')){
+      spelare2 ++
+      }
+      if (gameInfo.includes('Spelare 1')){
+      spelare1 ++
+      }
+    againButton = await $('.again-btn');
+    await againButton.click();
+    await sleep(2000)
+    beginButton = await $('.begin-btn');
+    await beginButton.click();
+    await sleep(sleepTime*2);
+    do {
+      await sleep(sleepTime*2)
+      gameInfo = await driver.findElement(by.css('h3')).getText()
+      }
+      while (! gameInfo.includes('vann'))
+      if (gameInfo.includes('Spelare 2')){
+      spelare2 ++
+      }
+      if (gameInfo.includes('Spelare 1')){
+      spelare1 ++
+      }
   });
 
   this.Then(/^the normal bot should win all games$/, async function () {
-    // Write code here that turns the phrase above into concrete actions
+    assert(spelare2 === 3, 'Boten förlorade mot idioten!')
   });
 
-
-
-
 }
-=======
- 
-  // Scenarios
- 
-  this.When(/^the first player plays (\d+) bricks in a row horizontally$/, async function (brickstoWin) {
- 
-    // NOTE: Only began this code, by playing one brick
-    let slots = await $('.slot'); 
-    // clicking slots[0] is putting a coin in column 1
-    // clicking slots[1] is putting a coin in column 2
-    await slots[3].click();
- 
-    await sleep(sleepTime * 10);
- 
-    // MORE TO WRITE HERE!
-  
-  });
- 
-
-}
->>>>>>> bf81c80ff4c15f62e150bdae4ac49b62f22ee1ff
