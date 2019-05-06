34eda03f883642efc498ddc67c343c993bb4cde8
eyeq1337 <eyeq24@gmail.com>
2019-05-06 09:40:26 +0200

    us06-09

--- four-with-bot/step-definitions/[US06]-Stupid-bot.js
+++ four-with-bot/step-definitions/[US06]-Stupid-bot.js
@@ -1,29 +1,25 @@
 // [US06] As a user I want the dumb bot to play worse than the normal bot
 // David
 
-let {$, sleep} = require('./funcs');
- 
+let { $, sleep } = require('./funcs');
+
 let sleepTime = 500;
-<<<<<<< HEAD
 let spelare1 = 'Normal bot'
 let spelare2 = 'Dumb bot'
 
 module.exports = function () {
 
-=======
- 
-module.exports = function(){
- 
->>>>>>> 078b2b1bb1abc721b79ea391f6fa16dc408c6f9b
   // Background
- 
-  this.Given(/^that I goto the game page$/, async function () {
-    await helpers.loadPage('https://localhost:3000/game');
-  });
-<<<<<<< HEAD
 
-  this.When(/^I choose a dumb bot to play against a normal bot$/, async function () {
+  this.Given(/^that I goto the game page$/, async function () {
+    await helpers.loadPage('http://localhost:3000/game');
+    await sleep(sleepTime * 2);
+
+  });
+
+  this.When(/^I choose to play as a dumb bot player vs\. a normal bot$/, async function () {
     let typeChoiceButton = await $('.type-choice-btn');
+
     await typeChoiceButton[0].click();
     let choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
     for (let choice of choices) {
@@ -67,91 +63,24 @@ module.exports = function(){
   this.Then(/^the game should start$/, async function () {
     let activeMenuLink = await $('.nav-link.active');
     let text = await activeMenuLink.getText();
-    await sleep(1000); // small wait needed
+    await sleep(200); // small wait needed
     assert.equal(text, 'Avbryt spelet', 'The game did not start!');
     await sleep(sleepTime * 2);
   });
+  // Scenario: Game
 
+  this.Then(/^they should play (\d+) games against each other$/, async function (brickstoWin) {
+    // Write code here that turns the phrase above into concrete actions
+    await sleep(sleepTime * 10000);
+    let againButton = await $('.again-btn');
+    await againButton.click();
+  });
+
+  this.Then(/^the normal bot should win all games$/, async function () {
+    // Write code here that turns the phrase above into concrete actions
+  });
 
 
 
 
 }
-=======
- 
-  this.When(/^choose to play as Dum Bot$/, async function () {
-    let typeChoiceButtons = await $('.type-choice-btn' );
-    for(let typeChoiceButton of typeChoiceButtons){
-      await typeChoiceButton.click();
-      let choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
-      for(let choice of choices){
-        let text = await choice.getText();
-        if(text === 'Dum Bot'){
-          await choice.click();
-          // we MUST break because the dom changes after click
-          // and erases the old menu.. (tricky...)
-          break;
-        }
-      }
-      await sleep(sleepTime * 2);
-    }
-  });
-
- });
-
-  this.When(/^choose to play as Bot$/, async function () {
-    let typeChoiceButtons = await $('.type-choice-btn' );
-    for(let typeChoiceButton of typeChoiceButtons){
-      await typeChoiceButton.click();
-      let choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
-      for(let choice of choices){
-        let text = await choice.getText();
-        if(text === 'Bot'){
-          await choice.click();
-          // we MUST break because the dom changes after click
-          // and erases the old menu.. (tricky...)
-          break;
-        }
-      }
-      await sleep(sleepTime * 2);
-
-  this.When(/^with two different names$/, async function () {
-    let inputFields = await $('input[placeholder="Namn (2-10 tecken)"]');
-    await inputFields[0].sendKeys('Spelare 1');
-    await sleep(sleepTime * 2);
-    await inputFields[1].sendKeys('Spelare 2');
-    await sleep(sleepTime * 2);
-  });
- 
-  this.When(/^press the Börja spela\-button$/, async function () {
-    let beginButton = await $('.begin-btn');
-    beginButton.click();
-    await sleep(sleepTime * 2);
-  });
- 
-  this.Then(/^the game should start$/, async function () {
-    let activeMenuLink = await $('.nav-link.active');
-    let text = await activeMenuLink.getText();
-    await sleep(1000); // small wait needed
-    assert.equal(text, 'Avbryt spelet', 'The game did not start!');
-    await sleep(sleepTime * 2);
-  });
- 
-  // Scenarios
- 
-  this.When(/^the first player plays (\d+) bricks in a row horizontally$/, async function (brickstoWin) {
- 
-    // NOTE: Only began this code, by playing one brick
-    let slots = await $('.slot'); 
-    // clicking slots[0] is putting a coin in column 1
-    // clicking slots[1] is putting a coin in column 2
-    await slots[3].click();
- 
-    await sleep(sleepTime * 10);
- 
-    // MORE TO WRITE HERE!
- 
-  });
- 
-}
->>>>>>> 078b2b1bb1abc721b79ea391f6fa16dc408c6f9b
