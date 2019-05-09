c44ad158231c486665dd703825436f2dd1158990
Danadude <dan.olsen85@gmail.com>
2019-05-09 09:40:31 +0200

    dan

--- four-with-bot/step-definitions/Follow-rules.js
+++ four-with-bot/step-definitions/Follow-rules.js
@@ -6,7 +6,7 @@ module.exports = function() {
     this.When(/^"([^"]*)" plays a bricks$/,async function (arg1) {
       let gameInfo = await driver.findElement(by.css('h3')).getText()
       await gameInfo
-      assert(gameInfo.includes('Spelare 1, drag 1') , 'Boten förlorade mot idioten!')
+      assert(gameInfo.includes('Spelare 1, drag 1') , 'wrong order!')
       clickRow(1)
 
       });
@@ -15,7 +15,7 @@ module.exports = function() {
         await sleep(sleepTime*6)
         let gameInfo = await driver.findElement(by.css('h3')).getText()
         await gameInfo
-        assert(gameInfo.includes('Spelare 2, drag 1') , 'Boten förlorade mot idioten!')
+        assert(gameInfo.includes('Spelare 2, drag 1') , 'wrong order!')
 
       });      
 
--- four-with-bot/step-definitions/[US06]-Stupid-bot.js
+++ four-with-bot/step-definitions/[US06]-Stupid-bot.js
@@ -1,95 +1,97 @@
 // [US06] As a user I want the dumb bot to play worse than the normal bot
 // David
 
-let { $, sleep } = require('./funcs');
-let spelare1 = 0
-let spelare2 = 0
+let { $, sleep } = require("./funcs");
+let spelare1 = 0;
+let spelare2 = 0;
 let sleepTime = 500;
 
-module.exports = function () {
-
+module.exports = function() {
   // Background
 
-  this.When(/^I choose to play as a dumb bot player vs\. a normal bot$/, async function () {
-    let typeChoiceButton = await $('.type-choice-btn');
+  this.When(
+    /^I choose to play as a dumb bot player vs\. a normal bot$/,
+    async function() {
+      let typeChoiceButton = await $(".type-choice-btn");
 
-    await typeChoiceButton[0].click();
-    let choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
-    for (let choice of choices) {
-      let text = await choice.getText();
-      if (text.includes("Dum")) {
-        await choice.click();
-        // we MUST break because the dom changes after click
-        // and erases the old menu.. (tricky...)
-        break;
+      await typeChoiceButton[0].click();
+      let choices = await $(".dropdown-menu.type-choice.show .dropdown-item");
+      for (let choice of choices) {
+        let text = await choice.getText();
+        if (text.includes("Dum")) {
+          await choice.click();
+          // we MUST break because the dom changes after click
+          // and erases the old menu.. (tricky...)
+          break;
+        }
+      }
+
+      await typeChoiceButton[1].click();
+      choices = await $(".dropdown-menu.type-choice.show .dropdown-item");
+      for (let choice of choices) {
+        text = await choice.getText();
+        if (text === "Bot") {
+          await choice.click();
+          // we MUST break because the dom changes after click
+          // and erases the old menu.. (tricky...)
+          break;
+        }
+        await sleep(sleepTime * 2);
       }
     }
+  );
 
-    await typeChoiceButton[1].click();
-    choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
-    for (let choice of choices) {
-      text = await choice.getText();
-      if (text === 'Bot') {
-        await choice.click();
-        // we MUST break because the dom changes after click
-        // and erases the old menu.. (tricky...)
-        break;
-      }
-      await sleep(sleepTime * 2);
-    }
-    });
-
-  this.Then(/^they should play (\d+) games against each other$/,{timeout: 90*1000}, async function (gamesToPlay) {
-    let gameInfo = await driver.findElement(by.css('h3')).getText()
+  this.Then(
+    /^they should play (\d+) games against each other$/,
+    { timeout: 90 * 1000 },
+    async function(gamesToPlay) {
+      let gameInfo = await driver.findElement(by.css("h3")).getText();
       do {
-      await sleep(sleepTime*2)
-      gameInfo = await driver.findElement(by.css('h3')).getText()
+        await sleep(sleepTime * 2);
+        gameInfo = await driver.findElement(by.css("h3")).getText();
+      } while (!gameInfo.includes("vann"));
+      if (gameInfo.includes("Spelare 2")) {
+        spelare2++;
       }
-      while (! gameInfo.includes('vann'))
-      if (gameInfo.includes('Spelare 2')){
-      spelare2 ++
+      if (gameInfo.includes("Spelare 1")) {
+        spelare1++;
       }
-      if (gameInfo.includes('Spelare 1')){
-      spelare1 ++
+      let againButton = await $(".again-btn");
+      await againButton.click();
+      await sleep(2000);
+      let beginButton = await $(".begin-btn");
+      await beginButton.click();
+      await sleep(sleepTime * 2);
+      do {
+        await sleep(sleepTime * 2);
+        gameInfo = await driver.findElement(by.css("h3")).getText();
+      } while (!gameInfo.includes("vann"));
+      if (gameInfo.includes("Spelare 2")) {
+        spelare2++;
       }
-    let againButton = await $('.again-btn');
-    await againButton.click();
-    await sleep(2000)
-    let beginButton = await $('.begin-btn');
-    await beginButton.click();
-    await sleep(sleepTime*2);
-    do {
-      await sleep(sleepTime*2)
-      gameInfo = await driver.findElement(by.css('h3')).getText()
+      if (gameInfo.includes("Spelare 1")) {
+        spelare1++;
       }
-      while (! gameInfo.includes('vann'))
-      if (gameInfo.includes('Spelare 2')){
-      spelare2 ++
+      againButton = await $(".again-btn");
+      await againButton.click();
+      await sleep(2000);
+      beginButton = await $(".begin-btn");
+      await beginButton.click();
+      await sleep(sleepTime * 2);
+      do {
+        await sleep(sleepTime * 2);
+        gameInfo = await driver.findElement(by.css("h3")).getText();
+      } while (!gameInfo.includes("vann"));
+      if (gameInfo.includes("Spelare 2")) {
+        spelare2++;
       }
-      if (gameInfo.includes('Spelare 1')){
-      spelare1 ++
+      if (gameInfo.includes("Spelare 1")) {
+        spelare1++;
       }
-    againButton = await $('.again-btn');
-    await againButton.click();
-    await sleep(2000)
-    beginButton = await $('.begin-btn');
-    await beginButton.click();
-    await sleep(sleepTime*2);
-    do {
-      await sleep(sleepTime*2)
-      gameInfo = await driver.findElement(by.css('h3')).getText()
-      }
-      while (! gameInfo.includes('vann'))
-      if (gameInfo.includes('Spelare 2')){
-      spelare2 ++
-      }
-      if (gameInfo.includes('Spelare 1')){
-      spelare1 ++
-      }
-  });
+    }
+  );
 
-  this.Then(/^the normal bot should win all games$/, async function () {
-    assert(spelare2 === 3, 'Boten förlorade mot idioten!')
+  this.Then(/^the normal bot should win all games$/, async function() {
+    assert(spelare2 === 3, "Boten förlorade mot idioten!");
   });
-
-}
+};
