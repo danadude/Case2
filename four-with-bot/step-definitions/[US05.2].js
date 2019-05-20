39288218b5da008eae52b555d7f903bbcc722ae8
tomasmansson <tomas.mansson93@gmail.com>
2019-05-20 10:29:35 +0200

    using checkIfGameIsFinished from funcs.js

--- four-with-bot/step-definitions/[US05.2].js
+++ four-with-bot/step-definitions/[US05.2].js
@@ -1,7 +1,7 @@
 // [US05] As a user I want the normal bot to have some randomness in its game choices, so that I can improve my own game
 // Tomas
 
-let { $, sleep, boardToArray } = require('./funcs')
+let { $, sleep, boardToArray, checkIfGameIsFinished } = require('./funcs')
 
 let sleepTime = 500
 
@@ -54,28 +54,7 @@ module.exports = function () {
   })
 
   this.Then(/^the normal bots will play a game against each other$/, { timeout: 240 * 1000 }, async function () {
-    while (true) {
-      let gameInfoH3 = await $('.game-info h3')
-      // if there is no h3 run next iteration of the loop
-      if (gameInfoH3 === null) { continue }
-      // otherwise check the text in the h3
-      let text
-      try {
-        text = await gameInfoH3.getText()
-      }
-      catch (e) {
-        // the element probably disappeared from the dom
-        // we go a selenium "stale element" error
-        // just continue the loop
-        continue
-      }
-      if (text.includes('oavgjort') || text.includes('vann')) {
-        // stop the loop if the game is over
-        break
-      }
-      // wait between checks
-      await sleep(200)
-    }
+    await checkIfGameIsFinished()
   })
 
   this.Then(/^they will place their bricks in a certain way$/, async function () {
@@ -83,32 +62,12 @@ module.exports = function () {
     gameOutcome1 = await boardToArray()
     // console.log(gameOutcome1.length)
     // console.log(gameOutcome1)
+    // assert, checking if board is saved as gameOutcome1
     assert(gameOutcome1.length === 42, "gameOutcome1.length should be 42")
   })
 
   this.Then(/^the normal bots will play a second game against each other$/, { timeout: 240 * 1000 }, async function () {
-    while (true) {
-      let gameInfoH3 = await $('.game-info h3')
-      // if there is no h3 run next iteration of the loop
-      if (gameInfoH3 === null) { continue }
-      // otherwise check the text in the h3
-      let text
-      try {
-        text = await gameInfoH3.getText()
-      }
-      catch (e) {
-        // the element probably disappeared from the dom
-        // we go a selenium "stale element" error
-        // just continue the loop
-        continue
-      }
-      if (text.includes('oavgjort') || text.includes('vann')) {
-        // stop the loop if the game is over
-        break
-      }
-      // wait between checks
-      await sleep(200)
-    }
+    await checkIfGameIsFinished()
   })
 
   this.Then(/^they should not play identically in comparison to the first game$/, async function () {
@@ -116,10 +75,10 @@ module.exports = function () {
     gameOutcome2 = await boardToArray()
     // console.log(gameOutcome2.length)
     // console.log(gameOutcome2)
+    // assert, checking if board is saved as gameOutcome1
+    assert(gameOutcome2.length === 42, "gameOutcome2.length should be 42")
     // assert for comparing game 1 board and game 1 board, 
     // if they are not equal the test will pass
-    assert(gameOutcome2.length === 42, "gameOutcome2.length should be 42")
     assert.notDeepEqual(gameOutcome1, gameOutcome2, "[The two boards are equal. They should not be equal]")
   })
-
 }
