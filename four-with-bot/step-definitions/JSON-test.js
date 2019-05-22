f056a86f86499d0934dec7d98b46c2998cadd021
ChristofferSundmanYoshida <christoffer.ekholm@gmail.com>
2019-05-22 09:23:03 +0200

    städade i json-test

--- four-with-bot/step-definitions/JSON-test.js
+++ four-with-bot/step-definitions/JSON-test.js
@@ -1,84 +1,15 @@
 let {$, sleep, clickCol, playGame} = require('./funcs');
- 
 let sleepTime = 500;
-
-//JSON TESTING GROUNDS!! BE AWARE!! LIVE AMMUNITION!!
-
-/*
-let fs = require('fs')
-let jsonTest = {
-    tabell: []
-}
-
-jsonTest.tabell.push({id: 1, win: "red"})
-let json = JSON.stringify(jsonTest)
-fs.writeFile('minjsonfil.json', json, 'utf8', callback)
-
-fs.readFile('minjsonfil.json', 'utf8', function readFileCallback(err, data){
-  if (err){
-      console.log(err);
-  } else {
-  jsonTest = JSON.parse(data); //now it an object
-  jsonTest.tabell.push({"id": 2, "win": "yellow"}); //add some data
-  json = JSON.stringify(jsonTest); //convert it back to json
-  fs.writeFile('minjsonfil.json', json, 'utf8', callback); // write it back 
-}});
-
-*/
-
-
-/*const flexjson = require('jsonflex')();
-const express = require('express');
-const app = express();
-app.use(flexjson);
-
-let persons = [{name:'Anna', name: 'Bob', name: 'Cecilia'}];
-let json = JSON.stringify(persons)
-JSON._save('persons.json', json).then(function(){
-  console.log('Saved!');
-});*/
-
-
-// DENNA NEDANFÖR FUNGERAR; IBLAND!
-
 const fs = require('fs');
  
-//tom tabell i ett objekt för att populera med data
+
 let winCounter = {
   vinnare: []
 };
  
-//Pusha in data i den tomma tabellen
-//winCounter.vinnare.push({spelare:0, resultat:"-"})
 
 // stringify JSON Object (för att kunna spara den sen)
 var jsonContent = JSON.stringify(winCounter); //tidigare jsonObj
-
-
-// parse json (detta behövs för att kunna addera data)
-//let jsonObj = JSON.parse(jsonContent);
-//console.log(jsonObj);
-
-//winCounter.vinnare.push({id:2, color:1})
-
-//Stringifiera igen så att den kan sparas som JSONfil
-//jsonContent = JSON.stringify(winCounter); 
-
-
-
-//Skriv och spara den som faktisk fil (output.json)
-
-/*fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
- 
-    console.log("JSON file has been saved.");
-});
-*/
-
-//JSON TESTING GROUNDS ENDS HERE!! EVERYTHING ABOVE UNTIL JSON TESTING GROUNDS START IS SAFE TO DELETE!!
 
 module.exports = function(){
   // Background
@@ -163,6 +94,5 @@ module.exports = function(){
 
 }
 
-//, callback
-//, function readFileCallback(err, data
+
 
